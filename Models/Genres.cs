ec3282ddb55baa5cb92dd852ec243645c8786766
Maria Arruda <Maria.Arruda@adidas.com>
2023-05-28 11:25:19 +0200

    Removes List and Genre Id

--- Models/Genres.cs
+++ Models/Genres.cs
@@ -3,8 +3,6 @@ using Books;
 
 public class Genres
 {
-    public int Id {get; set;}
     public string Genre {get; set;}
     public string Subgenre {get; set;}
-    public List<Books> book {get; set;}
 }
