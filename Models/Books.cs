7406ff70ceadfb109418181b55a4b21567d117c9
Maria Arruda <Maria.Arruda@adidas.com>
2023-05-28 11:08:26 +0200

    Add Book Model

---
+++ Models/Books.cs
@@ -0,0 +1,13 @@
+namespace Books;
+
+public class Books
+{
+
+    public int Id {get; set;}
+    public string Author {get; set;}
+    public string Genre {get; set;}
+    public decimal Price {get; set;}
+    public DateTime datePublished {get; set;}
+    public bool isInStock {get; set;}
+
+}
