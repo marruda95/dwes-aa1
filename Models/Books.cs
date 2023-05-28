af2b7ef3345e905fd6a9e45f0c3d7f1727ee7a91
Maria Arruda <Maria.Arruda@adidas.com>
2023-05-28 11:13:56 +0200

    Add List<Genres> to Books Model

--- Models/Books.cs
+++ Models/Books.cs
@@ -1,11 +1,11 @@
 namespace Books;
-
+using Genres;
 public class Books
 {
 
     public int Id {get; set;}
     public string Author {get; set;}
-    public string Genre {get; set;}
+    public List<Genres> genre {get; set;}
     public decimal Price {get; set;}
     public DateTime datePublished {get; set;}
     public bool isInStock {get; set;}
