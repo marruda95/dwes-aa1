b1ee05e11d900eb86cf9cbcd63a9e9cd60318e84
Maria Arruda <Maria.Arruda@adidas.com>
2023-05-28 11:25:33 +0200

    Add new Book to List

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿
 
+List<Books> books = new List<Books>();
+books.Add(new Books{
+    Id = "1",
+    Author = "Sarah J Maas",
+    Price = 21.00, 
+    Name = "A Court of Thorns and Roses",
+    datePublished = new DateTime(),
+    genre = new List<Genres>{
+        new Genres{Genres = "Fantasy"},
+        new Genres{Subgenre = "Romance"},
+        new Genres{Subrengre = "Young Adult"}
+    }
+});
